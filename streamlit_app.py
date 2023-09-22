de2fb049b52c541174ef23795f6ab75277b41ad3
Alexisbitecode <143207524+Alexisbitecode@users.noreply.github.com>
2023-09-21 21:00:30 -0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import io
+
+# Load data
+@st.cache
+def load_data():
+    df = pd.read_csv("Fish.csv")
+    return df
+
+df = load_data()
+
+# Group by species and calculate mean
+group_mean = df.groupby('Species').mean()
+
+# Display the bar chart using Streamlit
+st.bar_chart(group_mean['Weight'])
+
+# Customize the chart's appearance
+st.title("The average weight for each species")
+st.xlabel('Species')
+st.ylabel('Average weight')
+
