d75e13bec7d65bfa14b7a79662aa28fbba00c623
Alexisbitecode <143207524+Alexisbitecode@users.noreply.github.com>
2023-09-28 02:29:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,20 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns  # Add this import statement
 
 # Load the data from a CSV file
-df = pd.read_csv("Fish.csv")
+df = pd.read_csv("StudentsPerformance.csv")
+df1 = df.groupby("parental level of education").mean()
 
-# Create a scatter plot
-fig, ax = plt.subplots()
-
-ax.scatter(df['Weight'], df['Height'])
-
-# Set labels and title
-plt.xlabel('Weight')
-plt.ylabel('Height')
-plt.title('Scatter Plot: Weight vs. Height')
-
+fig = plt.figure(figsize=(10, 6))
+sns.scatterplot(x="math score", y="reading score", hue="parental level of education", data=df1)
+plt.xlabel("Math Score")
+plt.ylabel("Reading Score")
+plt.title("Scatter Plot of Math Score vs. Reading Score (Colored by Parental Education)")
 # Display the plot in Streamlit
 st.pyplot(fig)
+
 
 
 
